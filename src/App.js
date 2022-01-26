25270a690f059d178c11c03a3558f44ecf9e05b0
Danial Javanmardi <javanmardi.danial@gmail.com>
2022-01-26 01:31:46 -0800

    Add prettier

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import { useState, useEffect } from "react";
-import { BiCalendar } from "react-icons/bi"
-import Search from "./components/Search"
-import AddAppointment from "./components/AddAppointment"
-import "./App.css"
+import { BiCalendar } from "react-icons/bi";
+import Search from "./components/Search";
+import AddAppointment from "./components/AddAppointment";
+import "./App.css";
 
 const App = () => {
   const [appointmentList, setAppointmentList] = useState([]);
 
   const getAppointments = async () => {
-     const response = await fetch("data.json")
-     const data = await response.json();
-     setAppointmentList(data);
-  }
+    const response = await fetch("data.json");
+    const data = await response.json();
+    setAppointmentList(data);
+  };
   useEffect(() => {
     getAppointments();
   }, []);
@@ -19,17 +19,16 @@ const App = () => {
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl mb-3">
-        <BiCalendar className="inline-block text-red-400 align-top" />Your Appointments
+        <BiCalendar className="inline-block text-red-400 align-top" />
+        Your Appointments
       </h1>
       <AddAppointment />
       <Search />
-      {appointmentList.map(
-        appointment=>(
-          <h1 key={appointment.id}>{appointment.petName}</h1>
-        )
-      )}
+      {appointmentList.map((appointment) => (
+        <h1 key={appointment.id}>{appointment.petName}</h1>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
